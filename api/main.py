a30451917bd1c9a197fa24dfb70e45107304b81e
bryanambrosio <124814444+bryanambrosio@users.noreply.github.com>
2025-02-03 09:28:42 -0300

    Update main.py

--- api/main.py
+++ api/main.py
@@ -1,11 +1,25 @@
+import pickle
 from fastapi import FastAPI
+import pandas as pd
 
 app = FastAPI()
+
+# Carregar o modelo
+model_path = 'models/titanic_model.pkl'
+
+with open(model_path, 'rb') as model_file:
+    model = pickle.load(model_file)
 
 @app.get("/")
 def read_root():
     return {"message": "API do Titanic funcionando!"}
 
-@app.get("/predict")
-def predict():
-    return {"prediction": "Aqui ficará a previsão do modelo!"}
+@app.post("/predict")
+def predict(data: dict):
+    # Transforme o dicionário de entrada em DataFrame
+    df = pd.DataFrame([data])
+
+    # Realize a previsão com o modelo carregado
+    prediction = model.predict(df)
+
+    return {"prediction": prediction.tolist()}
